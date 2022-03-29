23e51de5c3477b29a72005abc7f737cb161563cf
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-29 00:36:10 +0000

    march 28th

--- server.js
+++ server.js
@@ -2,4 +2,28 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'ahumareze',
+    api_key: '169527325266592',
+    api_secret: 'nrkLCaqONtnbA9z_DBr63N2mDLs'
+});
+
+const uploads = (file, folder) => {
+    return new Promise(resolve => {
+        cloudinary.uploader.upload(file, (result) => {
+            resolve({
+                url: result.url,
+                id: result.public_id
+            })
+            console.log(resolve)
+        })
+    })
+}
+
+app.post('/upload', (req, res) => {
+    console.log(req.body.file)
+})
+
 app.listen(PORT)
