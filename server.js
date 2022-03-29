4281819b20e3d5daecd98b70cbe72ae246a651eb
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-29 12:46:08 +0000

    march 29th

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const {cloudinary} = require('./utils/cloudinary');
-
+const Crime = require('./models/crime');
 const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
